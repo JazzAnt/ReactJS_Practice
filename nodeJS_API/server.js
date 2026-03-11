aa51ca9c9ecc23ce5be2a041a4b88351bfa77136
Jason A. Januar <jason.kwee98@gmail.com>
2026-03-11 13:39:08 -0700

    setup EJS view engine

--- nodeJS_API/server.js
+++ nodeJS_API/server.js
@@ -7,11 +7,15 @@ const app = express();
 /* Setup Middleware to parse body requests */
 app.use(express.urlencoded({ extended: true }));
 
-/* To get directory */
+/* To get directory (used for EJS) */
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+/* Set up EJS view engine */
+app.set("views", path.join(__dirname, "/views"))
+app.set("view engine", "ejs")
 
 /* Create Listener */
 const PORT = 3000;
